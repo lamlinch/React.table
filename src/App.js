4457502ae32594479aa0436d4e2ab443be032f36
lamlinch <khoshamanhamed@gmail.com>
2021-12-08 00:54:03 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,132 @@
+import React, { useState } from 'react';
+import './App.css';
+import MaterialTable from 'material-table'
+import XLSX from 'xlsx'
+
+const EXTENSIONS = ['xlsx', 'xls', 'csv']
+function App() {
+  const [colDefs, setColDefs] = useState()
+  const [data, setData] = useState()
+
+  const getExention = (file) => {
+    const parts = file.name.split('.')
+    const extension = parts[parts.length - 1]
+    return EXTENSIONS.includes(extension) // return boolean
+  }
+
+  const convertToJson = (headers, data) => {
+    const rows = []
+    data.forEach(row => {
+      let rowData = {}
+      row.forEach((element, index) => {
+        rowData[headers[index]] = element
+      })
+      rows.push(rowData)
+
+    });
+    return rows
+  }
+
+  const importExcel = (e) => {
+    const file = e.target.files[0]
+
+    const reader = new FileReader()
+    reader.onload = (event) => {
+      //parse data
+
+      const bstr = event.target.result
+      const workBook = XLSX.read(bstr, { type: "binary" })
+
+      //get first sheet
+      const workSheetName = workBook.SheetNames[0]
+      const workSheet = workBook.Sheets[workSheetName]
+      //convert to array
+      const fileData = XLSX.utils.sheet_to_json(workSheet, { header: 1 })
+      // console.log(fileData)
+      const headers = fileData[0]
+      const heads = headers.map(head => ({ title: head, field: head }))
+      setColDefs(heads)
+
+      //removing header
+      fileData.splice(0, 1)
+
+
+      setData(convertToJson(headers, fileData))
+    }
+
+    if (file) {
+      if (getExention(file)) {
+        reader.readAsBinaryString(file)
+      }
+      else {
+        alert("Invalid file input, Select Excel, CSV file")
+      }
+    } else {
+      setData([])
+      setColDefs([])
+    }
+  }
+
+  return (
+    <div className="App">
+      <input type="file" onChange={importExcel} />
+      <MaterialTable title="" data={data} columns={colDefs}   options={{
+
+        exportButton: true,        
+        exportFileName:"Download",
+        search: false,
+        
+      }}
+       localization={{
+        body:{
+          emptyDataSourceMessage:"",
+        },
+        toolbar:{
+          addRemoveColumns:'add',
+          exportTitle:'Download',
+
+        },
+        header:{
+          actions:''
+        },
+        pagination:{
+          labelDisplayedRows:'{count} satırdan {from}-{to} arası',
+        }
+       }}
+       editable={{
+        onRowAdd: newData =>
+          new Promise((resolve, reject) => {
+            setTimeout(() => {
+              setData([...data, newData]);
+              
+              resolve();
+            }, 1000)
+          }),
+        onRowUpdate: (newData, oldData) =>
+          new Promise((resolve, reject) => {
+            setTimeout(() => {
+              const dataUpdate = [...data];
+              const index = oldData.tableData.id;
+              dataUpdate[index] = newData;
+              setData([...dataUpdate]);
+
+              resolve();
+            }, 1000)
+          }),
+        onRowDelete: oldData =>
+          new Promise((resolve, reject) => {
+            setTimeout(() => {
+              const dataDelete = [...data];
+              const index = oldData.tableData.id;
+              dataDelete.splice(index, 1);
+              setData([...dataDelete]);
+              
+              resolve()
+            }, 1000)
+          }),
+      }} />
+    </div>
+  );
+}
+
+export default App;
